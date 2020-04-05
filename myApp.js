61e87340206e3596d33896a578518ed17339a776
Drozerah <drozerah@gmail.com>
2020-04-05 14:13:30 +0200

    feat: sets Content Security Policy header

--- myApp.js
+++ myApp.js
@@ -178,7 +178,12 @@ app.use(helmet.noCache())
 // in the `"'self'"` keyword, the single quotes are part of the keyword itself, 
 // so it needs to be enclosed in **double quotes** to be working.
 
-
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    styleSrc: ["'self'", 'https://cdn.hyperdev.com']
+  }
+}))
 
 /** TIP: */ 
 
